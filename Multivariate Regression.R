d88301f86b64bc3d629df7819bab334bb722b6b3
tangirala29 <31567144+tangirala29@users.noreply.github.com>
2017-12-13 09:56:13 +0530

    Add files via upload

---
+++ Multivariate Regression.R
@@ -0,0 +1,6 @@
+# Multivariate Regression
+# Dependent Variable: Price
+# Independent Variable: Mileage, Make, Cylinder, Liter, Cruise, Sound, Leather
+
+Regression<- lm(Price~Mileage+Make+Cylinder+Liter+Cruise+Sound+Leather, data=)
+summary(Regression)
